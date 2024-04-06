b2a0d0bb833acf15645ce2eb468b677dd251e249
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-04-06 08:50:34 +0700

    changed color in footer links

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,12 +13,12 @@
 
         <div class="flex gap-8">
             <button>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-[30px] h-[30px]">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-[25px] h-[25px]">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
                 </svg>
             </button>
             <button>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-[30px] h-[30px]">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-[25px] h-[25px]">
                     <path stroke-linecap="round" stroke-linejoin="round" d="m10.5 21 5.25-11.25L21 21m-9-3h7.5M3 5.621a48.474 48.474 0 0 1 6-.371m0 0c1.12 0 2.233.038 3.334.114M9 5.25V3m3.334 2.364C11.176 10.658 7.69 15.08 3 17.502m9.334-12.138c.896.061 1.785.147 2.666.257m-4.589 8.495a18.023 18.023 0 0 1-3.827-5.802" />
                 </svg>
             </button>
@@ -70,7 +70,7 @@
                 <p class="mt-6">The tiredness that you feel now is better than the one you feel later</p>
 
                 <!-- socials links -->
-                <div class="flex mt-8 gap-x-[20px]">
+                <div class="flex mt-8 gap-x-8">
                     <a href="https://github.com/AlvinSetyaPranata">
                         <svg fill="currentColor" viewBox="0 0 24 24" class="w-[30px] h-[30px] fill-gray-300"><path fill-rule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clip-rule="evenodd"></path></svg>
                     </a>
@@ -80,7 +80,7 @@
                 </div>            
             </div>
 
-            <div class="flex gap-x-20 text-white">
+            <div class="flex gap-x-20 text-gray-500">
                 <div class="flex flex-col gap-y-6">
                     <p class="font-semibold">Pages</p>
                     <a href="#">Home</a>
@@ -90,8 +90,8 @@
                 <div class="flex flex-col gap-y-6">
                     <p class="font-semibold">Supports</p>
                     <a href="#">Api References</a>
-                    <a href="#">About me</a>
-                    <a href="#">Contact</a>
+                    <a href="#">Help</a>
+                    <a href="#">Comunity</a>
                 </div>
             </div>
         </div>
